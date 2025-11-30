fd7a970c18640b79982b64d3f115298f8f8de8ab
to0l-b4s3 <shinnon.anashe@gmail.com>
2025-11-30 19:22:22 +0000

    deleted unnecessary files

--- Sample-BaseBot/src/utils/sendMessageWTyping.js
+++
@@ -1,8 +0,0 @@
-export const sendMessageWTyping = async (sock, msg, jid) => {
-  await sock.presenceSubscribe(jid);
-  await delay(500);
-  await sock.sendPresenceUpdate("composing", jid);
-  await delay(2000);
-  await sock.sendPresenceUpdate("paused", jid);
-  return await sock.sendMessage(jid, msg);
-};
